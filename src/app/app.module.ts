e1c6630935437d5fb6a6f8cb1b93b80846042834
t1jlavila <jlavila@todo1.net>
2018-10-23 08:32:11 -0500

    starting the bae

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

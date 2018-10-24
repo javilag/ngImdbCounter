53d51aec0d08ae67a01faf745505ba8c80e8422b
t1jlavila <jlavila@todo1.net>
2018-10-24 15:16:43 -0500

    adding the s in safe

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,20 +13,20 @@ export class DataService {
   }
 
   getABunchOfBatmanMovies () {
-    return this.http.get<Object[]>('http://www.omdbapi.com/?s=' + this.idMovie + '&apikey=37a27720');
+    return this.http.get<Object[]>('https://www.omdbapi.com/?s=' + this.idMovie + '&apikey=37a27720');
   }
 
   getAMovieByName (movieName: string) {
-    return this.http.get<Object[]>('http://www.omdbapi.com/?s=' + movieName + '&type=series&apikey=37a27720');
+    return this.http.get<Object[]>('https://www.omdbapi.com/?s=' + movieName + '&type=series&apikey=37a27720');
   }
 
   selectingASpecificRuntime (imdbId: string) {
-    const gettingMovie = this.http.get<any>('http://www.omdbapi.com/?i=' + imdbId + '&page=1&apikey=37a27720');
+    const gettingMovie = this.http.get<any>('https://www.omdbapi.com/?i=' + imdbId + '&page=1&apikey=37a27720');
     return gettingMovie;
   }
 
   gettingTheRealNumberChapters (imdbId: string, cuantSeason: number) {
-    const gettingMovie = this.http.get<any>('http://www.omdbapi.com/?i=' + imdbId + '&Season=' + cuantSeason + '&apikey=37a27720');
+    const gettingMovie = this.http.get<any>('https://www.omdbapi.com/?i=' + imdbId + '&Season=' + cuantSeason + '&apikey=37a27720');
     return gettingMovie;
   }
 }
